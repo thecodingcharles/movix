d74f7cbd2617c590161104f1996ef5a5458cfc27
Carlos Perez Araujo <Charles@Diegos-MacBook-Air-3.local>
2020-04-10 19:37:59 -0500

    Downloaded Spring Boot Projects

---
+++ movie-info-service/src/test/java/com/anothercodingcharles/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anothercodingcharles.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ movie-rating-service/src/test/java/com/anothercodingcharles/movieratingservice/MovieRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anothercodingcharles.movieratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
